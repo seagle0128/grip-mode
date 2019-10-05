e76ac3d7294e57b37e8eeeb953f436b26ade730f
Vincent Zhang <seagle0128@gmail.com>
2019-10-06 00:04:48 +0800

    Bump version.

--- grip-mode.el
+++ grip-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Vincent Zhang <seagle0128@gmail.com>
 ;; Homepage: https://github.com/seagle0128/grip-mode
-;; Version: 2.1.0
+;; Version: 2.1.1
 ;; Package-Requires: ((emacs "24.4"))
 ;; Keywords: convenience, markdown, preview
 
