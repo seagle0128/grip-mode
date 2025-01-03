df0ba7589db7c54c4b68bd6a96a246676d7893c3
Peter H. Hoeg <peter@hoeg.com>
2025-01-03 21:20:13 +0100

    TIL that derived-mode-p takes a list

--- grip-mode.el
+++ grip-mode.el
@@ -266,8 +266,7 @@ Use default browser unless `xwidget' is available."
     (add-hook 'kill-emacs-hook #'grip-stop-preview nil t)
     (cond ((derived-mode-p 'org-mode)
            (grip--preview-org))
-          ((or (derived-mode-p 'markdown-mode)
-               (derived-mode-p 'markdown-ts-mode))
+          ((derived-mode-p '(markdown-mode markdown-ts-mode))
            (grip--preview-md))
           (t
            (grip-mode -1)
